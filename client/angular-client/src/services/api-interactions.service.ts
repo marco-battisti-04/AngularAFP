acdc7198daa69fd6661d86fcfcca7b409cb6b966
KemonoBAT4 <kemono.bat.4@gmail.com>
2025-04-03 22:59:45 +0200

    [angular] updated web utils services

--- client/angular-client/src/services/api-interactions.service.ts
+++ client/angular-client/src/services/api-interactions.service.ts
@@ -13,4 +13,22 @@ export class ApiInteractionsService {
     let server_response = await this.webUtils.get(`/search/${query}`);
     console.log(server_response)
   }
+
+  async getLibrary(query: string) {
+    let response = {
+      message: 'Failed to fetch library data',
+      status: 400
+    }
+    try {
+      if (query.trim() == '') {
+        response = await this.webUtils.get<>(`/library/all`);
+      } else {
+        response = await this.webUtils.get<>(`/library/title/${query}`);
+      }
+    } catch (error) {
+      // console.error('Error fetching library data:', error);
+      response = { error: 'Failed to fetch library data' };
+    }
+    return response;
+  }
 }
--- client/angular-client/src/services/web-utils.service.ts
+++ client/angular-client/src/services/web-utils.service.ts
@@ -1,28 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebUtilsService {
-  private api_url = "127.0.0.1:5000/api/v1"
+  private api_url = environment.api_url
 
   constructor(private http: HttpClient) { }
 
   get<T>(endpoint: string, options?: { headers?: HttpHeaders }): Observable<T> {
-    return this.http.get<T>(`${this.api_url}${endpoint}`, options);
+    return this.http.get<T>(`${this.api_url}${endpoint}`, options).pipe(
+      map((response: T) => response)
+    );
   }
 
   post<T>(endpoint: string, body: any, options?: { headers?: HttpHeaders }): Observable<T> {
-    return this.http.post<T>(`${this.api_url}${endpoint}`, body, options);
+    return this.http.post<T>(`${this.api_url}${endpoint}`, body, options).pipe(
+      map((response: T) => response)
+    );
   }
 
   put<T>(endpoint: string, body: any, options?: { headers?: HttpHeaders }): Observable<T> {
-    return this.http.put<T>(`${this.api_url}${endpoint}`, body, options);
+    return this.http.put<T>(`${this.api_url}${endpoint}`, body, options).pipe(
+      map((response: T) => response)
+    );
   }
 
   delete<T>(endpoint: string, options?: { headers?: HttpHeaders }): Observable<T> {
-    return this.http.delete<T>(`${this.api_url}${endpoint}`, options);
+    return this.http.delete<T>(`${this.api_url}${endpoint}`, options).pipe(
+      map((response: T) => response)
+    );
   }
 }
